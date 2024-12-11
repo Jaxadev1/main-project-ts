42f0be5ce7903cd13eff4a3ae97929a1d65bdd41
Jahongir Aminov <jahongiraminov@Mac.mshome.net>
2024-12-11 15:56:03 +0500

    commit

--- main-project/src/components/Header.tsx
+++ main-project/src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header: React.FC = () => {
   return (
     <header>
-      <div className="container">header</div>
+      <div className="container">Header</div>
     </header>
   );
 };
